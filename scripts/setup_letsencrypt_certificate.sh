84080b17630d4b996b1c52b6cfa27bbca9375d09
Paula <pmarjim089@g.educaand.es>
2025-10-23 10:24:28 +0000

    certbot

--- scripts/setup_letsencrypt_certificate.sh
+++ scripts/setup_letsencrypt_certificate.sh
@@ -2,3 +2,12 @@
 set -ex
 #Importamos el archivo .env
 source .env
+#Realizamos la instalación y actualización de snapd y nginx
+snap install core
+snap refresh core
+#Eliminamos cualquier versión anterior de certbot
+apt remove certbot -y
+# Instalamos cliente Certbot con snapd
+snap install --classic certbot
+# Obtenemos el certificado y configuramos el servidor Apache
+sudo certbot --apache -m $CERTBOT_EMAIL --agree-tos --no-eff-email -d $CERTBOT_DOMAIN --non-interactive
