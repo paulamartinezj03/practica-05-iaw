913577b34cad1e8535adab56b605cd1859ad0f99
Paula <pmarjim089@g.educaand.es>
2025-10-24 08:25:30 +0000

    cambios conf

--- scripts/setup_letsencrypt_certificate.sh
+++ scripts/setup_letsencrypt_certificate.sh
@@ -2,6 +2,11 @@
 set -ex
 #Importamos el archivo .env
 source .env
+
+#Copiamos la "plantilla" del archivo de configuración de apache
+cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
+#Modificamos el valor del Servername
+sed -i "s/PUT_YOUR_DOMAIN_HERE/$CERTBOT_DOMAIN/" /etc/apache2/sites-available/000-default.conf
 #Realizamos la instalación y actualización de snapd y nginx
 snap install core
 snap refresh core
